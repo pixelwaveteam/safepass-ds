6c589322fdafcc50620d612c8039dbbc5a3ff3a5
Ruver Dornelas <ruverd@outboxup.com>
2024-04-03 23:32:10 -0300

    Update package.json and remove unnecessary files

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -5,11 +5,6 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
