73834cc94047192526cb6bc8a52192e1a34f5c90
JamDev0 <jamdeveloper0@gmail.com>
2024-04-08 03:45:16 -0300

    feat: typography's heading component

---
+++ packages/react/src/components/typography/heading.tsx
@@ -0,0 +1,51 @@
+import { cva } from 'class-variance-authority';
+import { ElementType, HTMLAttributes, forwardRef } from 'react';
+
+const headingVariants = cva('font-bold', {
+  variants: {
+     size: {
+      default: 'text-sm uppercase',
+      sm: 'text-sm',
+      md: 'text-md',
+      lg: 'text-lg',
+      xl: 'text-xl',
+      '2xl': 'text-5xl',
+      '4xl': 'text-4xl',
+     },
+  },
+  defaultVariants: {
+    size: 'default',
+  },
+})
+
+interface HeadingProps extends HTMLAttributes<typeof HTMLHeadingElement> {
+  size?: 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '4xl';
+  as?: ElementType; // HTMLHeadingElement;
+}
+
+const DEFAULT_HEADING_AS = 'h2';
+
+export const Heading = forwardRef<HTMLHeadElement, HeadingProps>(
+  (
+    {
+      as: HeadingComponent = DEFAULT_HEADING_AS,
+      size,
+      className,
+      children,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <HeadingComponent
+        className={headingVariants({ size, className })}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </HeadingComponent>
+    );
+  }
+)
+
+Heading.displayName = 'Heading';
