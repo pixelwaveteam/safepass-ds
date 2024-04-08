2e04d3fd82780d2cae2a16ea8fd9918dd3902e1b
JamDev0 <jamdeveloper0@gmail.com>
2024-04-08 02:12:40 -0300

    feat: add tailwind utility class absolutely-centered

--- packages/react/tailwind.config.ts
+++ packages/react/tailwind.config.ts
@@ -1,10 +1,23 @@
 import { Config } from 'tailwindcss';
 
+import plugin from 'tailwindcss/plugin';
 import { shadcnPreset } from './src/lib/shadcn-preset';
 
 const config = {
   presets: [shadcnPreset],
   content: ['./src/**/*.{ts,tsx}'],
+  plugins: [
+    plugin(function({ addUtilities }) {
+      addUtilities({
+        '.absolutely-centered': {
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }
+      })
+    })
+  ]
 } satisfies Config;
 
 export default config;
